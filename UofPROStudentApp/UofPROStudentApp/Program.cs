e67fe5ae1b491c8d4f0945547a13f90e37530506
Rigney <krigney@proforma.com>
2018-04-15 16:39:35 -0400

    Started structure for students' app.

---
+++ UofPROStudentApp/UofPROStudentApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UofPROStudentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CONNECTION_TO_DATABASE = @"Server=localhost\SQLEXPRESS;Database=UniversityofPROforma;Trusted_Connection=True;";
+
+        }
+    }
+}
---
+++ UofPROStudentApp/UofPROStudentApp/Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UofPROStudentApp
+{
+    class Students
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Major { get; set; }
+
+        public Students()
+        {
+
+        }
+
+        public Students(SqlDataReader reader)
+        {
+            ID = (int)reader["ID"];
+            FullName = reader["FullName"].ToString();
+            Email = reader["Email"].ToString();
+            PhoneNumber = reader["PhoneNumber"].ToString();
+            Major = reader["Major"].ToString();
+        }
+    }
+}
